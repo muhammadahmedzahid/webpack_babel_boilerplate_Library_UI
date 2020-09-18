8a107238cd48f124aaa6101d94ff6f8d3af4904e
muhammadahmed zahid <ahmedzahid212121@gmail.com>
2020-09-18 15:04:22 +0500

    after tooltip complete

---
+++ src/UI_files/tooltip.js
@@ -0,0 +1,24 @@
+// We have to import the css class here
+import './styles/Tooltip.css'
+class Tooltip{
+    constructor(element){
+        this.element = element;
+        this.message = element.getAttribute('data-message');
+    }
+    init(){
+        const tip = document.createElement('div');
+        tip.classList.add('tip');
+        tip.textContent = this.message;
+        this.element.appendChild(tip);
+
+        this.element.addEventListener('mouseenter', () => {
+            tip.classList.add('active');
+        });
+
+        this.element.addEventListener('mouseleave', () => {
+            tip.classList.remove('active');
+        });
+    }
+}
+
+export {Tooltip as default};
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+console.log('test');
+// in order to add the dependency for the css to override the previous method.
+// run the command {npm install css-loader style-loader --save-dev}
+// the code runs from right to left in the webpack.config.js file in the array.
+// after importing the data we are gonna import this file in index also.
+// ./ is used to specify the current directory
+// import './test.css'
+import Tooltip from './UI_files/tooltip';
 
+// create a tooltip
+
+const tooltip = new Tooltip(document.querySelector('.tooltip'));
+
+tooltip.init();
